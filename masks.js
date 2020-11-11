492ea62d1a40e6950514905acbc9819925c24bf9
Gustavo Shigueo <58121396+gustavo-shigueo@users.noreply.github.com>
2020-11-10 21:28:57 -0300

    Update masks.js

--- masks.js
+++ masks.js
@@ -34,56 +34,56 @@ function maskInput(target) {
 function maskFone(id) {
 	const element = document.getElementById(id) // Seleciona o input correto com base no ID
 	element.setAttribute('maxLength', '15')     // Limita o número de caracteres do input
-	element.value = element.value				// Aplica a máscara fazendo uso de expressões regulares
-						.replace(/\D/g, '')
-						.replace(/^(\d{2})(\d)/g, "($1) $2")
-						.replace(/(\d)(\d{4})$/, "$1-$2")
+	element.value = element.value		    // Aplica a máscara fazendo uso de expressões regulares
+			    .replace(/\D/g, '')
+			    .replace(/^(\d{2})(\d)/g, "($1) $2")
+			    .replace(/(\d)(\d{4})$/, "$1-$2")
 }
 
 function maskCPF(id) {
 	const element = document.getElementById(id)
 	element.setAttribute('maxLength', '14')
 	element.value = element.value
-						.replace(/\D/g, '')
-						.replace(/^(\d{3})(\d)/g, "$1.$2")
-						.replace(/(\d{3}\.\d{3})(\d)/g, "$1.$2")
-						.replace(/(\d{3}\.\d{3}\.\d{3})(\d|\d{2})$/, "$1-$2")
+			    .replace(/\D/g, '')
+			    .replace(/^(\d{3})(\d)/g, "$1.$2")
+			    .replace(/(\d{3}\.\d{3})(\d)/g, "$1.$2")
+			    .replace(/(\d{3}\.\d{3}\.\d{3})(\d|\d{2})$/, "$1-$2")
 }
 
 function maskRG(id) {
 	const element = document.getElementById(id)
 	element.setAttribute('maxLength', '12')
 	element.value = element.value
-						.replace(/\D/g, '')
-						.replace(/^(\d{2})(\d)/g, "$1.$2")
-						.replace(/(\d{2}\.\d{3})(\d)/g, "$1.$2")
-						.replace(/(\d{2}\.\d{3}\.\d{3})(\d)$/, "$1-$2")
+			    .replace(/\D/g, '')
+			    .replace(/^(\d{2})(\d)/g, "$1.$2")
+			    .replace(/(\d{2}\.\d{3})(\d)/g, "$1.$2")
+			    .replace(/(\d{2}\.\d{3}\.\d{3})(\d)$/, "$1-$2")
 }
 function maskCEP(id) {
 	const element = document.getElementById(id)
 	element.setAttribute('maxLength', '9')
 	element.value = element.value
-						.replace(/\D/g, '')
-						.replace(/^(\d{5})(\d)/g, "$1-$2")
+			    .replace(/\D/g, '')
+			    .replace(/^(\d{5})(\d)/g, "$1-$2")
 }
 
 function maskData(id) {
 	const element = document.getElementById(id)
 	element.setAttribute('maxLength', '10')
 	element.value = element.value
-						.replace(/\D/g, '')
-						.replace(/^(\d{2})(\d)/g, "$1/$2")
-						.replace(/^(\d{2}\/\d{2})(\d)/g, "$1/$2")
+			    .replace(/\D/g, '')
+			    .replace(/^(\d{2})(\d)/g, "$1/$2")
+			    .replace(/^(\d{2}\/\d{2})(\d)/g, "$1/$2")
 }
 
 function maskMoeda(id) {
 	const element = document.getElementById(id)
 	element.setAttribute('maxLength', '23')
 	element.value = 'R$ ' + 
-						(Number(element.value.replace(/\D/g, '')) / 100)
-						.toFixed(2)
-						.toString()
-						.replace('.', ',')
-						.replace(/(\d)(?=(\d{3})+\,)/g, "$1.")
+			    (Number(element.value.replace(/\D/g, '')) / 100)
+			    .toFixed(2)
+			    .toString()
+			    .replace('.', ',')
+			    .replace(/(\d)(?=(\d{3})+\,)/g, "$1.")
 	if (element.value === 'R$ 0,00') element.value = ''
 }
