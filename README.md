eade85a17b5462b9a069692375dce2a4041d2474
gustavo-shigueo <gustavo.gsmn@gmail.com>
2020-11-10 21:11:53 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Mascáras para inputs de formulários HTML
+Esse script permite a utilização de máscaras em inputs HTML **sem a necessidade de usar jQuery.**
+Se tiver sugestões de mais tipos de máscaras, crie um _issue_ neste repositório.
+Este repositório contém um arquivo HTML e um arquivo CSS para demonstração. Esses arquivos não são necessários, apenas o arquivo masks.js é importante.
+
+## Como usar?
+Para adicionar as máscaras, basta criar um arquivo HTML, adicionar a tag <script src="masks.js" defer></script>, direcionando o atributo src ao arquivo masks.js.
+ Depois, nas tags <input type="text">, **é necessário adicionar um id** qualquer (desde que não haja mais elementos com o mesmo id no arquivo HTML) **e um atributo data-mask.**
+ O valor de data-mask irá determinar o tipo de máscara aplicada.
+
+## Máscaras disponíveis:
+* fone: (00) 00000-0000 ou (00) 0000-0000
+* cpf: 000.000.000-00
+* rg: 00.000.000-0
+* cep: 00000-000
+* data: 00/00/0000
+* moeda: de R$ 0,00 até R$ 0.000.000.000.000,00
+
+## Uso no HTML:
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <script src="masks.js" defer></script>
+    </head>
+    <body>
+        <input type="text" id="exemplo1" data-mask="fone">
+        <input type="text" id="exemplo2" data-mask="cpf">
+        <input type="text" id="exemplo3" data-mask="rg">
+        <input type="text" id="exemplo4" data-mask="cep">
+        <input type="text" id="exemplo5" data-mask="data">
+        <input type="text" id="exemplo6" data-mask="moeda">
+    </body>
+</html>
---
+++ masks.js
@@ -0,0 +1,89 @@
+// Armazena todos os elementos do DOM que tiverem o atributo data-mask definido
+const fields = [...document.querySelectorAll('[data-mask]')]
+
+// Adiciona Event Listeners a todos os items do array fields
+for (let field of fields) {
+	field.addEventListener('onKeyPress', maskInput)
+	field.addEventListener('input'     , maskInput)
+}
+
+function maskInput(target) {
+	if (target.type !== 'input') return // Rejeita o elemento caso não seja um input
+	const dataType = target.srcElement.getAttribute('data-mask') // Obtém o valor de data-mask
+	const id       = target.srcElement.getAttribute('id')        // Obtém o ID do elemento
+	
+	// Aciona a função adequada de acordo com o dataType, passando o ID como parâmetro
+	switch(dataType){
+		case 'fone': maskFone(id)
+			break
+		case 'cpf': maskCPF(id)
+			break
+		case 'rg': maskRG(id)
+			break
+		case 'cep': maskCEP(id)
+			break
+		case 'data': maskData(id)
+			break
+		case 'moeda': maskMoeda(id)
+			break
+		default: return
+	}
+
+}
+
+function maskFone(id) {
+	const element = document.getElementById(id) // Seleciona o input correto com base no ID
+	element.setAttribute('maxLength', '15')     // Limita o número de caracteres do input
+	element.value = element.value				// Aplica a máscara fazendo uso de expressões regulares
+						.replace(/\D/g, '')
+						.replace(/^(\d{2})(\d)/g, "($1) $2")
+						.replace(/(\d)(\d{4})$/, "$1-$2")
+}
+
+function maskCPF(id) {
+	const element = document.getElementById(id)
+	element.setAttribute('maxLength', '14')
+	element.value = element.value
+						.replace(/\D/g, '')
+						.replace(/^(\d{3})(\d)/g, "$1.$2")
+						.replace(/(\d{3}\.\d{3})(\d)/g, "$1.$2")
+						.replace(/(\d{3}\.\d{3}\.\d{3})(\d|\d{2})$/, "$1-$2")
+}
+
+function maskRG(id) {
+	const element = document.getElementById(id)
+	element.setAttribute('maxLength', '12')
+	element.value = element.value
+						.replace(/\D/g, '')
+						.replace(/^(\d{2})(\d)/g, "$1.$2")
+						.replace(/(\d{2}\.\d{3})(\d)/g, "$1.$2")
+						.replace(/(\d{2}\.\d{3}\.\d{3})(\d)$/, "$1-$2")
+}
+function maskCEP(id) {
+	const element = document.getElementById(id)
+	element.setAttribute('maxLength', '9')
+	element.value = element.value
+						.replace(/\D/g, '')
+						.replace(/^(\d{5})(\d)/g, "$1-$2")
+}
+
+function maskData(id) {
+	const element = document.getElementById(id)
+	element.setAttribute('maxLength', '10')
+	element.value = element.value
+						.replace(/\D/g, '')
+						.replace(/^(\d{2})(\d)/g, "$1/$2")
+						.replace(/^(\d{2}\/\d{2})(\d)/g, "$1/$2")
+}
+
+function maskMoeda(id) {
+	const element = document.getElementById(id)
+	element.setAttribute('maxLength', '23')
+	element.value = 'R$ ' + 
+						(Number(element.value.replace(/\D/g, '')) / 100)
+						.toFixed(2)
+						.toString()
+						.replace('.', ',')
+						.replace(/(\d)(?=(\d{3})+\,)/g, "$1.")
+	if (element.value === 'R$ 0,00') element.value = ''
+}
