4f9f33ca3dcd301ccf30a496eaddc77579a0d6b4
gustavo-shigueo <gustavo.gsmn@gmail.com>
2020-11-27 18:34:17 -0300

    Criando máscaras personalizadas

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Para adicionar as máscaras, basta criar um arquivo HTML, adicionar a tag `<scri
  Depois, nas tags `<input type="text">`, **é necessário adicionar um id** qualquer (desde que não haja mais elementos com o mesmo id no arquivo HTML) **e um atributo data-mask.**
  O valor de data-mask irá determinar o tipo de máscara aplicada.
 
-## Máscaras disponíveis:
+## Máscaras padrão disponíveis:
 * fone: (00) 00000-0000 ou (00) 0000-0000
 * cpf: 000.000.000-00
 * rg: 00.000.000-0
@@ -16,6 +16,11 @@ Para adicionar as máscaras, basta criar um arquivo HTML, adicionar a tag `<scri
 * data: 00/00/0000
 * moeda: de R$ 0,00 até R$ 0.000.000.000.000,00
 * uf: AA (permite apenas a digitação de siglas de estados brasileiros, ex: SP, RJ, DF)
+
+## Máscaras personalizadas:
+Agora é possível criar suas próprias máscaras! Basta digitar o padrão desejado dentro do atributo **data-mask**.
+A criação de padrões usa zeros como caracteres numéricos e símbolos digitados pelo usuário como máscara, no entanto há uma limitação: Não é possível usar dois caracteres não numéricos juntos.
+Exemplo: a máscara do tipo fone não pode ser recriada com essa função, pois usao formato '(00) 00000-0000', que contém um parênteses ')' e um espaço em branco ' ' adjascentes.
 
 ## Uso no HTML:
 ```html
@@ -32,6 +37,7 @@ Para adicionar as máscaras, basta criar um arquivo HTML, adicionar a tag `<scri
         <input type="text" id="exemplo5" data-mask="data">
         <input type="text" id="exemplo6" data-mask="moeda">
         <input type="text" id="exemplo7" data-mask="uf">
+        <input type="text" id="exemplo8" data-mask="000 00-0.0@000!000">
     </body>
 </html>
 ```
--- masks.js
+++ masks.js
@@ -25,7 +25,7 @@ function maskInput(event) {
 			break
 		case 'uf': maskUF(id)
 			break
-		default: return
+		default: maskGeneral(id)
 	}
 }
 
@@ -97,4 +97,41 @@ function maskUF(id) {
 						.toUpperCase()                                                            // Converte em letras maiúsculas
 						.replace(new RegExp(`^[^${primeiro}]$`, 'g'), '')                         // Filtra o 1º caracter
 						.replace(new RegExp(`^([A-Z])([^${regexes[element.value[0]]}])$`), '$1')  // Filtra o 2º caracter
+}
+
+function maskGeneral(id) {
+	const element = document.getElementById(id)
+	const mask = element.getAttribute('data-mask').trim()
+
+	element.setAttribute('maxLength', `${mask.length}`)
+
+	let maskSymbols = []
+	let l = 0
+	for (const char of mask) {
+		if (char !== '0') {
+			if (l > 0) maskSymbols.push(l)
+			maskSymbols.push(char)
+			l = 0
+		} 
+		else l++
+	}
+
+	apply()
+
+	function apply() {
+		element.value = element.value.replace(/\D/g, '')
+		let replaces = []
+
+		for (let i = typeof(maskSymbols[0]) === 'string' ? 0 : 1; i < maskSymbols.length; i += 2) {
+			let regex = replaces.length !== 0 ? `${replaces[replaces.length - 1][0].replace('})', '}')}\\${maskSymbols[i - 2]}` : ``
+			if (i === 0) replaces.push(['^(\\d)', `${maskSymbols[i]}$1`])
+			else if (i % 2 === 0)	regex = i === 2 ? `^(\\${maskSymbols[0]}\\d{${maskSymbols[1]}})` : `${regex}\\d{${maskSymbols[i - 1]}})`
+			else regex += `${i === 1 ? '(' : ''}\\d{${maskSymbols[i - 1]}})`
+
+			if (i !== 0) replaces.push([regex, `$1${maskSymbols[i]}`])
+
+			if (typeof(maskSymbols[i]) === 'string' && typeof(maskSymbols[i + 1]) === 'string') console.error(`Não é possível usar dois caracteres não numéricos juntos nas máscaras pesonalizadas.\nVerifique o atributo data-mask do elemento #${id} e tente novamente`)
+		}
+		for (const replace of replaces) element.value = element.value.replace(new RegExp(`${replace[0]}(\\d)`), `${replace[1]}$2`)
+	}
 }
