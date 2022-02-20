1c2900efbc5ff9ad70ebd5ce77ac1015259bed68
gustavo-shigueo <gustavo.gsmn@gmail.com>
2022-02-20 16:26:17 -0300

    Adicionado máscara para CNPJ e validações para CPF, CNPJ e datas

--- README.md
+++ README.md
@@ -1,42 +1,61 @@
 # Mascáras para inputs de formulários HTML
+
 Esse script permite a utilização de máscaras em inputs HTML **sem a necessidade de usar jQuery.**
 Se tiver sugestões de mais tipos de máscaras, crie um _issue_ neste repositório.
 Este repositório contém um arquivo HTML e um arquivo CSS para a página de demonstração, que pode ser acessada [aqui](https://gustavo-shigueo.github.io/mascara/). Esses arquivos não são necessários, apenas o arquivo masks.js é.
 
 ## Como usar?
+
 Para adicionar as máscaras, basta criar um arquivo HTML, adicionar a tag `<script src="masks.js" defer></script>` , direcionando o atributo src ao arquivo masks.js.
- Depois, nas tags `<input type="text">`, **é necessário adicionar um id** qualquer (desde que não haja mais elementos com o mesmo id no arquivo HTML) **e um atributo data-mask.**
- O valor de data-mask irá determinar o tipo de máscara aplicada.
+Depois, nas tags `<input type="text">`,
+O valor de data-mask irá determinar o tipo de máscara aplicada.
 
 ## Máscaras padrão disponíveis:
-* fone: (00) 00000-0000 ou (00) 0000-0000
-* cpf: 000.000.000-00
-* rg: 00.000.000-0
-* cep: 00000-000
-* data: 00/00/0000
-* moeda: de R$ 0,00 até R$ 0.000.000.000.000,00
-* uf: AA (permite apenas a digitação de siglas de estados brasileiros, ex: SP, RJ, DF)
+
+- fone: (00) 00000-0000 ou (00) 0000-0000
+- cpf: 000.000.000-00
+- cnpj: 00.000.000/0000-00
+- rg: 00.000.000-0
+- cep: 00000-000
+- data: 00/00/0000
+- moeda: de R$ 0,00 até R$ 0.000.000.000.000,00
+- uf: AA (permite apenas a digitação de siglas de estados brasileiros, ex: SP, RJ, DF)
+
+## Validação de datas
+
+Campos com `data-mask="data"` agora serão automáticamente validados para o formato `dd/mm/aaaa`
+
+## Validadores de CPF e CNPJ
+
+Agora campos com `data-mask="cpf"` e `data-mask="cnpj"` serão validados automaticamente e impedirão o formulário de ser enviado caso as validações falhem
+
+### Sobre validação de RG
+
+RGs ainda não são consistentes em nível nacional, e mesmo que venham a ser, eles não têm data de expiração, portanto pessoas com RGs antigos podem ter documentos que não sigam a norma, portanto validar RGs costuma ser uma má ideia
 
 ## Máscaras personalizadas:
+
 Agora é possível criar suas próprias máscaras! Basta digitar o padrão desejado dentro do atributo **data-mask**.
 A criação de padrões usa zeros como caracteres numéricos e símbolos digitados pelo usuário como máscara.
 
 ## Uso no HTML:
+
 ```html
 <!DOCTYPE html>
 <html lang="pt-BR">
-    <head>
-        <script src="masks.js" defer></script>
-    </head>
-    <body>
-        <input type="text" id="exemplo1" data-mask="fone">
-        <input type="text" id="exemplo2" data-mask="cpf">
-        <input type="text" id="exemplo3" data-mask="rg">
-        <input type="text" id="exemplo4" data-mask="cep">
-        <input type="text" id="exemplo5" data-mask="data">
-        <input type="text" id="exemplo6" data-mask="moeda">
-        <input type="text" id="exemplo7" data-mask="uf">
-        <input type="text" id="exemplo8" data-mask="000 00-0.0@000!000">
-    </body>
+	<head>
+		<script src="masks.js" defer></script>
+	</head>
+	<body>
+		<input type="text" data-mask="fone" />
+		<input type="text" data-mask="cpf" />
+		<input type="text" data-mask="cnpj" />
+		<input type="text" data-mask="rg" />
+		<input type="text" data-mask="cep" />
+		<input type="text" data-mask="data" />
+		<input type="text" data-mask="moeda" />
+		<input type="text" data-mask="uf" />
+		<input type="text" data-mask="000 00-0.0@000!000" />
+	</body>
 </html>
 ```
--- masks.js
+++ masks.js
@@ -1,134 +1,272 @@
 // Armazena todos os elementos do DOM que tiverem o atributo data-mask definido
-const fields = document.querySelectorAll('[data-mask]')
+const fields = [...document.querySelectorAll('[data-mask]')]
 
 // Adiciona Event Listeners a todos os items do array fields
-for (let field of fields) field.addEventListener('input', maskInput)
+fields.forEach(field => field.addEventListener('input', maskInput))
 
 function maskInput(event) {
-	if (event.target.tagName !== 'INPUT') return            // Rejeita o elemento caso não seja um input
-	const dataType = event.target.getAttribute('data-mask') // Obtém o valor de data-mask
-	const id       = event.target.getAttribute('id')        // Obtém o ID do elemento
+	/** @type {HTMLInputElement} */
+	const target = event.target
+
+	if (target.tagName !== 'INPUT') return // Rejeita o elemento caso não seja um input
+	const dataType = target.getAttribute('data-mask').toLowerCase() // Obtém o valor de data-mask
+
+	target.setCustomValidity('')
 
 	// Aciona a função adequada de acordo com o dataType, passando o ID como parâmetro
-	switch(dataType){
-		case 'fone': maskFone(id)
+	switch (dataType) {
+		case 'fone':
+			maskFone(target)
 			break
-		case 'cpf': maskCPF(id)
+		case 'cpf':
+			maskCPF(target)
 			break
-		case 'rg': maskRG(id)
+		case 'cnpj':
+			maskCNPJ(target)
 			break
-		case 'cep': maskCEP(id)
+		case 'rg':
+			maskRG(target)
 			break
-		case 'data': maskData(id)
+		case 'cep':
+			maskCEP(target)
 			break
-		case 'moeda': maskMoeda(id)
+		case 'data':
+			maskData(target)
 			break
-		case 'uf': maskUF(id)
+		case 'moeda':
+			maskMoeda(target)
 			break
-		default: maskGeneral(id)
+		case 'uf':
+			maskUF(target)
+			break
+		default:
+			maskGeneral(target)
+			break
 	}
+
+	target.reportValidity()
 }
 
-function maskFone(id) {
-	const element = document.getElementById(id) // Seleciona o input correto com base no ID
-	element.setAttribute('maxLength', '15')     // Limita o número de caracteres do input
-	element.value = element.value               // Aplica a máscara fazendo uso de expressões regulares
-						.replace(/\D/g, '')
-						.replace(/^(\d{2})(\d)/g, "($1) $2")
-						.replace(/(\d)(\d{4})$/, "$1-$2")
+function maskFone(element) {
+	element.setAttribute('maxLength', '15') // Limita o número de caracteres do input
+	element.value = element.value // Aplica a máscara fazendo uso de expressões regulares
+		.replace(/\D/g, '')
+		.replace(/^(\d{1,2})$/g, '($1')
+		.replace(/^(\d{2})(\d)/g, '($1) $2')
+		.replace(/(\d)(\d{4})$/, '$1-$2')
 }
 
-function maskCPF(id) {
-	const element = document.getElementById(id)
+/** @param {string} cpf */
+const validarCPF = cpf => {
+	const cpfDigitado = cpf.replace(/\D/g, '')
+	const cpfNums = cpfDigitado.split('').map(char => parseInt(char))
+
+	if (cpfNums.every(i => i === cpfNums[0])) return false
+
+	const soma1 = cpfNums.reduce(
+		(acc, cur, i) => (i === 9 ? acc : acc + cur * (10 - i)),
+		0
+	)
+
+	let primeiroDigito = (soma1 * 10) % 11
+	if (primeiroDigito > 9) primeiroDigito = 0
+
+	if (cpfNums[9] !== primeiroDigito) return false
+
+	const soma2 = cpfNums.reduce((acc, cur, i) => {
+		if (i === 10) return acc
+		return acc + cur * (11 - i)
+	}, 0)
+
+	let segundoDigito = (soma2 * 10) % 11
+	if (segundoDigito > 9) segundoDigito = 0
+
+	return cpfNums[10] === segundoDigito
+}
+
+/** @param {HTMLInputElement} element */
+function maskCPF(element) {
 	element.setAttribute('maxLength', '14')
 	element.value = element.value
-						.replace(/\D/g, '')
-						.replace(/(\d{3})(?=\d)(?<!.{9,})/g, "$1.")
-						.replace(/^(.{11})(\d+)$/, "$1-$2")
+		.replace(/\D/g, '')
+		.replace(/(\d{3})(?=\d)(?<!.{9,})/g, '$1.')
+		.replace(/^(.{11})(\d+)$/, '$1-$2')
+
+	if (element.value.length < 14) return
+
+	element.setCustomValidity(validarCPF(element.value) ? '' : 'CPF inválido')
 }
 
-function maskRG(id) {
-	const element = document.getElementById(id)
+/** @param {string} cnpj */
+const validarCNPJ = cnpj => {
+	const cnpjDigitado = cnpj.replace(/\D/g, '')
+	const cnpjNums = cnpjDigitado.split('').map(i => parseInt(i))
+
+	if (cnpjNums.every(i => i === cnpjNums[0])) return false
+
+	const soma1 = cnpjNums.reduce((acc, cur, i) => {
+		const c = i < 4 ? 5 : 13
+
+		return i > 11 ? acc : acc + cur * (c - i)
+	}, 0)
+
+	const modulo1 = soma1 % 11
+	const primeiroDigito = modulo1 < 2 ? 0 : 11 - modulo1
+
+	if (primeiroDigito !== cnpjNums[12]) return false
+
+	const soma2 = cnpjNums.reduce((acc, cur, i) => {
+		const c = i < 5 ? 6 : 14
+
+		return i > 12 ? acc : acc + cur * (c - i)
+	}, 0)
+
+	const modulo2 = soma2 % 11
+	const segundoDigito = modulo2 < 2 ? 0 : 11 - modulo2
+
+	return cnpjNums[13] === segundoDigito
+}
+
+/** @param {HTMLInputElement} element */
+function maskCNPJ(element) {
+	element.setAttribute('maxLength', '18')
+	element.value = element.value
+		.replace(/\D/g, '')
+		.replace(/^(\d{2})(\d)/, '$1.$2')
+		.replace(/^(\d{2}\.\d{3})(\d)/, '$1.$2')
+		.replace(/^(\d{2}\.\d{3}\.\d{3})(\d)/, '$1/$2')
+		.replace(/^(\d{2}\.\d{3}\.\d{3}\/\d{4})(\d)/, '$1-$2')
+
+	if (element.value.length < 18) return
+	element.setCustomValidity(validarCNPJ(element.value) ? '' : 'CNPJ inválido')
+}
+
+function maskRG(element) {
 	element.setAttribute('maxLength', '12')
 	element.value = element.value
-						.replace(/\D/g, '')
-						.replace(/(^\d{2}|\d{3})(?=\d)(?<!\d{8,})/g, "$1.")
-						.replace(/^(.{10})(\d)$/, "$1-$2")
+		.replace(/\D/g, '')
+		.replace(/(^\d{2}|\d{3})(?=\d)(?<!\d{8,})/g, '$1.')
+		.replace(/^(.{10})(\d)$/, '$1-$2')
 }
 
-function maskCEP(id) {
-	const element = document.getElementById(id)
+function maskCEP(element) {
 	element.setAttribute('maxLength', '9')
 	element.value = element.value
-						.replace(/\D/g, '')
-						.replace(/^(\d{5})(\d)/g, "$1-$2")
+		.replace(/\D/g, '')
+		.replace(/^(\d{5})(\d)/g, '$1-$2')
 }
 
-function maskData(id) {
-	const element = document.getElementById(id)
+function maskData(element) {
 	element.setAttribute('maxLength', '10')
 	element.value = element.value
-						.replace(/\D/g, '')
-						.replace(/(\d{2})(?=\d)(?<!.{6,})/g, "$1/")
+		.replace(/\D/g, '')
+		.replace(/^[4-9]$/, '')
+		.replace(/^(0)0$/, '$1')
+		.replace(/^(3)([2-9])/, '$1')
+		.replace(/^(\d{2})[2-9]/, '$1')
+		.replace(/^(\d{2}0)0/, '$1')
+		.replace(/^(\d{2}1)[3-9]/, '$1')
+		.replace(/^(\d{4})0/, '$1')
+		.replace(/(\d{2})(?=\d)(?<!.{6,})/g, '$1/')
+
+	if (element.value.length < 10) return
+
+	const [day, month, year] = element.value.split('/').map(i => parseInt(i))
+	const date = new Date(year, month - 1, day)
+
+	element.value = date.toLocaleDateString()
 }
 
-function maskMoeda(id) {
-	const element = document.getElementById(id)
+function maskMoeda(element) {
 	element.setAttribute('maxLength', '23')
-	element.value = 'R$ ' + 
-						(Number(element.value.replace(/\D/g, '')) / 100)
-						.toFixed(2)
-						.toString()
-						.replace('.', ',')
-						.replace(/(\d)(?=(\d{3})+\,)/g, "$1.")
+	element.value =
+		'R$ ' +
+		(Number(element.value.replace(/\D/g, '')) / 100)
+			.toFixed(2)
+			.toString()
+			.replace('.', ',')
+			.replace(/(\d)(?=(\d{3})+\,)/g, '$1.')
 	if (element.value === 'R$ 0,00') element.value = ''
 }
 
-function maskUF(id) {
+function maskUF(element) {
 	// Combinações possíveis de sigla (a chave representa o primeiro caracter e o valor representa as possibilidades do segundo)
 	// Exemplo {S: 'CEP'} significa que as combinações 'SC', 'SE' e 'SP' são siglas válidas
-	const regexes = { A: 'CLMP', B: 'A', C: 'E', D: 'F', E: 'S', G: 'O', M: 'AGST', P: 'ABEIR', R: 'JNORS', S: 'CEP', T: 'O' }
+	const regexes = {
+		A: 'CLMP',
+		B: 'A',
+		C: 'E',
+		D: 'F',
+		E: 'S',
+		G: 'O',
+		M: 'AGST',
+		P: 'ABEIR',
+		R: 'JNORS',
+		S: 'CEP',
+		T: 'O',
+	}
+
 	let primeiro = ''
 	for (let key in regexes) primeiro += key
-	const element = document.getElementById(id)
 	element.setAttribute('maxLength', '2')
 	element.value = element.value
-						.replace(/[^A-Za-z]/g, '')                                                // Permite apenas letras
-						.toUpperCase()                                                            // Converte em letras maiúsculas
-						.replace(new RegExp(`^[^${primeiro}]$`, 'g'), '')                         // Filtra o 1º caracter
-						.replace(new RegExp(`^([A-Z])([^${regexes[element.value[0]]}])$`), '$1')  // Filtra o 2º caracter
+		.replace(/[^A-Za-z]/g, '') // Permite apenas letras
+		.toUpperCase() // Converte em letras maiúsculas
+		.replace(new RegExp(`^[^${primeiro}]$`, 'g'), '') // Filtra o 1º caracter
+		.replace(new RegExp(`^([A-Z])([^${regexes[element.value[0]]}])$`), '$1') // Filtra o 2º caracter
 }
 
-function maskGeneral(id) {
-	const element = document.getElementById(id)
+function maskGeneral(element) {
 	const mask = element.getAttribute('data-mask').trim()
+	const maskSymbols = []
 
 	element.setAttribute('maxLength', `${mask.length}`)
 
-	let maskSymbols = []
 	let l = 0
 	for (const char of mask) {
-		if (char !== '0') {
-			maskSymbols.push(l, char)
-			l = 0
-		} 
-		else l++
+		if (char === '0') {
+			l++
+			continue
+		}
+
+		maskSymbols.push(l, char)
+		l = 0
 	}
 
 	apply()
 
 	function apply() {
 		element.value = element.value.replace(/\D/g, '')
-		let replaces = []
+		const replaces = []
 
-		for (let i = typeof(maskSymbols[0]) === 'string' ? 0 : 1; i < maskSymbols.length; i += 2) {
-			let regex = replaces.length !== 0 ? `${replaces[replaces.length - 1][0].replace('})', '}')}\\${maskSymbols[i - 2]}` : ``
-			if (i === 0) replaces.push(['^(\\d)', `${maskSymbols[i]}$1`])
-			else if (i % 2 === 0)	regex = i === 2 ? `^(\\${maskSymbols[0]}\\d{${maskSymbols[1]}})` : `${regex}\\d{${maskSymbols[i - 1]}})`
-			else regex += `${i === 1 ? '(' : ''}\\d{${maskSymbols[i - 1]}})`
+		const start = typeof maskSymbols[0] === 'string' ? 0 : 1
+		for (let i = start; i < maskSymbols.length; i += 2) {
+			const symbol = maskSymbols[i - 2]
+			const replacement = replaces[replaces.length - 1]?.[0]?.replace('})', '}')
+			let regex = replaces.length !== 0 ? `${replacement}\\${symbol}` : ''
+
+			switch (i) {
+				case 0:
+					replaces.push(['^(\\d)', `${maskSymbols[i]}$1`])
+					break
+				case 2:
+					regex = `^(\\${maskSymbols[0]}\\d{${maskSymbols[1]}})`
+					break
+				default:
+					regex +=
+						i % 2 === 0
+							? `\\d{${maskSymbols[i - 1]}})`
+							: `${i === 1 ? '(' : ''}\\d{${maskSymbols[i - 1]}})`
+			}
 
 			if (i !== 0) replaces.push([regex, `$1${maskSymbols[i]}`])
 		}
-		for (const replace of replaces) element.value = element.value.replace(new RegExp(`${replace[0]}(\\d)`), `${replace[1]}$2`)
+
+		for (const [reg, group] of replaces) {
+			element.value = element.value.replace(
+				new RegExp(`${reg}(\\d)`),
+				`${group}$2`
+			)
+		}
 	}
 }
